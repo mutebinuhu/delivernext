06b315a9e9cbb7961b5c7fe7d886bfc9fb9e87d2
mutebinuhu <mutebinuhu1@gmail.com>
2023-10-02 15:28:16 +0400

    chore: few changes

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Welcome to <span className='text-3xl bg-orange-500 px-4 text-gray-100 rounded-xl'> Deliver Next</span></h2>
+      <h2>Welcome to <span className='text-2xl bg-orange-500 px-4 text-gray-100 rounded-xl md:text-xl sm:text-xl'> Deliver Next</span></h2>
     </main>
   )
 }

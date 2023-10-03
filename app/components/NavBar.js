ad9fb6c1884c6b195dcd46c68b5345fdf5b705e5
mutebinuhu <mutebinuhu1@gmail.com>
2023-10-03 08:50:36 +0400

    core: add logo

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CiDeliveryTruck } from "react-icons/ci"
 
 const NavBar = () => {
     return (
@@ -7,7 +8,9 @@ const NavBar = () => {
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="#" className="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+                   <div className='mr-2 '>
+                        <CiDeliveryTruck size="40"  />
+                   </div>
                     <span className="text-md self-center sm:text-2xl font-semibold whitespace-nowrap dark:text-white">Deliver Next</span>
                 </a>
                 <div className="flex md:order-2">

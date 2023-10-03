1e5e3aecd00429456f8ac1cedb2be482e652c335
mutebinuhu <mutebinuhu1@gmail.com>
2023-10-03 08:36:21 +0400

    chore: few changes

--- app/page.js
+++ app/page.js
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <NavBar/>
+      <div className='flex items-center justify-center h-screen'>
+        <h1 className='text-5xl'>Coming Soon....</h1>
+      </div>
     </main>
   )
 }

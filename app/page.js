a54b460c725cea81e2d2d2d7d7f2290180e73fe8
mutebinuhu <mutebinuhu1@gmail.com>
2023-10-03 06:52:35 +0400

    chore: add The navbar

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import NavBar from './components/NavBar'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Welcome to <span className='text-2xl bg-orange-500 px-4 text-gray-100 rounded-xl md:text-xl sm:text-xl'> Deliver Next</span></h2>
+    <main className="min-h-screen">
+      <NavBar/>
     </main>
   )
 }

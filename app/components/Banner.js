a487bbd84feb44830b8ea67d6569f4f9fb40cdab
mutebinuhu <mutebinuhu1@gmail.com>
2023-10-04 08:24:37 +0400

    chore: add the banner

---
+++ app/components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div
+        className='bg-red-200'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad libero incidunt possimus eum laboriosam distinctio tempora ex. Dicta impedit sed voluptatum perspiciatis asperiores, soluta neque tenetur reprehenderit sint accusamus reiciendis?
+        </div>
+    );
+}
+
+export default Banner;
--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import NavBar from './components/NavBar'
+import Banner from './components/Banner'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <NavBar/>
-      <div className='flex items-center justify-center h-screen'>
-        <h1 className='text-5xl'>Coming Soon....</h1>
-      </div>
+      <section className=''>
+        <Banner/>
+
+      </section>
     </main>
   )
 }

6de35bc57a8d20d20f7681f5d5f8d47798e72046
mutebinuhu <mutebinuhu1@gmail.com>
2023-10-09 08:38:09 +0400

    core: add basic delivery page

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,25 @@
+"use client"
 import React from 'react';
-
+import { motion } from "framer-motion";
 const Page = () => {
     return (
-        <div>
-            <h1>Welcome to Our Dashboard</h1>
+        <div className="min-h-screen flex py-12">
+
+            <motion.div
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5 }}
+                className="flex-1 p-8"
+            >
+                <h1 className="text-2xl font-bold mb-4">Delivery Dashboard</h1>
+
+              
+
+            
+            </motion.div>
         </div>
     );
-}
+};
+
 
 export default Page;

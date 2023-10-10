52803f420ab25699521df73dc74aec3c29e2aac4
mutebinuhu <mutebinuhu1@gmail.com>
2023-10-10 07:07:09 +0400

    chore: few changes

--- app/api/auth/callback/route.js
+++ app/api/auth/callback/route.js
@@ -3,6 +3,7 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
+    console.log("GET", request)
     const url = new URL(request.url);
     console.log("url", url);
     const code = url.searchParams.get('code');
@@ -10,6 +11,6 @@ export const GET = async (request) => {
         let supabase = createRouteHandlerClient({ cookies })
         await supabase.auth.exchangeCodeForSession(code);
     }
-    //redirect Link
-    return NextResponse.redirect(process.env.NEXT_PUBLIC_PRODUCTION_URL+"/dashboard");
+
+    return NextResponse.redirect(url.origin+"/dashboard");
 }

166ea50755b137d123478549e9972e28534dc43f
mutebinuhu <mutebinuhu1@gmail.com>
2023-10-10 06:05:33 +0400

    chore: fix redirect to url

--- app/api/auth/callback/route.js
+++ app/api/auth/callback/route.js
@@ -11,5 +11,5 @@ export const GET = async (request) => {
         await supabase.auth.exchangeCodeForSession(code);
     }
 
-    return NextResponse.redirect(url.origin+"/dashboard");
+    return NextResponse.redirect(process.env.NEXT_LOCAL_URL+"/dashboard");
 }
---
+++ app/middleware.js
@@ -0,0 +1,8 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+export const middleware = async (req)=>{
+const res = NextResponse.next();
+const supabase = createMiddlewareClient({req, res})
+await supabase.auth.getSession();
+return res;
+}

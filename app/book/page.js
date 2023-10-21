76fd4361595d5fab3dc3bdf3a4b2eab86f6d08c6
mutebinuhu <mutebinuhu1@gmail.com>
2023-10-21 20:16:58 +0400

    chore: few changes

--- app/book/page.js
+++ app/book/page.js
@@ -3,7 +3,7 @@ import ShipmentBooking from '../components/ShipmentBooking';
 
 const Page = () => {
     return (
-        <div className='bg-gradient-to-br from-black via-gray-900 to-gray-800 min-h-screen flex items-center justify-center'>
+        <div className='bg-gradient-to-br w-full from-purple-400 via-pink-500 to-red-500 min-h-screen flex items-center justify-center'>
             <ShipmentBooking/>
         </div>
     );

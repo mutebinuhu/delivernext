4d8e0df0e32480324a552c18f12eea1ec0ff7817
@swaggp <mutebinuhu1@gmail.com>
2023-11-07 05:28:33 -0800

    chore: manage links

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -2,22 +2,27 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
+import { useRouter, usePathname } from 'next/navigation'
 
-const getUrl = () =>{
- return window.location.href
-}
-const redirectHome = () =>{
-   
- alert("im here");
 
-}
+const dashboardLinks = ['/dashboard', '/book']
+
 
 const NavLinks = () =>{
-    if (getUrl() === "http://localhost:3000/" || getUrl() === "https://delivernext.vercel.app/") {
+const pathName = usePathname();
+const router = useRouter();
+const redirectHome = () =>{
+if(pathName == "/"){
+router.push("/")
+}
+router.push("/dashboard")
+
+}
+    if (pathName == "/") {
         return (
         <>
             <li onClick={redirectHome}>
-                <Link href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-primary md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
+                <Link href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-primary md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
             </li>
             <li>
                 <Link href="#about" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>

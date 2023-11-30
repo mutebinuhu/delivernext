3506ada859ab9569fbec167b4988bea55b0f86c2
swaggp <mutebinuhu1@gmail.com>
2023-11-30 14:29:42 +0400

    chore: add few changes

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -18,14 +18,14 @@ const Page = () => {
     };
     return (
         <div className="min-h-screen w-auto">
-
+ <Hero/>
             <div
                 initial={{ opacity: 0, x: -50 }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.5 }}
                 className="px-8"
             >
-                <Hero/>
+               
 
                 {
                     /**

669ddd1a21f0e488374bff1641ad74186811b836
swaggp <mutebinuhu1@gmail.com>
2023-12-06 12:17:31 +0400

    chore: few changes

--- app/components/ShipmentBooking.js
+++ app/components/ShipmentBooking.js
@@ -136,7 +136,7 @@ const ShipmentBooking = ({showPage}) => {
                     <div className='md-flex w-full'>
                       
                         <h2 className="text-2xl font-semibold mb-4 mt-4 ">Book a Shipment</h2>
-                    {!isSubmitted && <p className='py-3 text-green-500'>Booking Sent Successfully 😊</p>}
+                    {isSubmitted && <p className='py-3 text-green-500'>Booking Sent Successfully 😊</p>}
                     {<p>{isLoading && 
 <div role="status">
     <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
